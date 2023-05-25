a1bf0e46cfc95ee0fc90cc7ba5c6a88b3e2f088f
div <div@justcommunication.ru>
2023-05-25 18:17:12 +1000

    De-Lombok-ization

--- domain/src/main/java/org/exemple/data/BookDto.java
+++ domain/src/main/java/org/exemple/data/BookDto.java
@@ -1,14 +1,5 @@
 package org.exemple.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class BookDto {
 
     private Long id;
@@ -18,4 +9,46 @@ public class BookDto {
     private String description;
 
     private Double price;
+
+    public BookDto() {
+    }
+
+    public BookDto(Long id, String title, String description, Double price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
--- infrastructure/src/main/java/org/example/entity/Book.java
+++ infrastructure/src/main/java/org/example/entity/Book.java
@@ -1,22 +1,12 @@
 package org.example.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +16,46 @@ public class Book {
     private String description;
 
     private Double price;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String description, Double price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
